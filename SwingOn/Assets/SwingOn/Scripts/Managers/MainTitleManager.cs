055be3d39652e63d358b3374f6fa74904cc8455f
han12c36 <112372556+han12c36@users.noreply.github.com>
2022-12-12 02:09:44 +0900

    Update
    Update

--- SwingOn/Assets/SwingOn/Scripts/Managers/MainTitleManager.cs
+++ SwingOn/Assets/SwingOn/Scripts/Managers/MainTitleManager.cs
@@ -6,15 +6,12 @@ public class MainTitleManager : Manager<MainTitleManager>
 {
     private void Awake()
     {
-        Debug.Log("메인타이틀 매니저 어웨이크");
     }
     private void OnEnable()
     {
-        Debug.Log("메인타이틀 매니저 온에이블");
     }
     private void Start()
     {
-        Debug.Log("메인타이틀 매니저 스타트");
     }
 
     private void Update()
@@ -22,6 +19,5 @@ public class MainTitleManager : Manager<MainTitleManager>
     }
     private void OnDisable()
     {
-        Debug.Log("메인타이틀 매니저 디스에이블");
     }
 }
