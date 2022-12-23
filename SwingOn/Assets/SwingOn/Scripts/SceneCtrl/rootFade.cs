ecad709d42fab00def78eed080d7dc71ef3b1a51
han12c36 <112372556+han12c36@users.noreply.github.com>
2022-12-23 17:59:39 +0900

    Update
    Update

--- SwingOn/Assets/SwingOn/Scripts/SceneCtrl/rootFade.cs
+++ SwingOn/Assets/SwingOn/Scripts/SceneCtrl/rootFade.cs
@@ -6,14 +6,16 @@ using UnityEngine.UI;
 public class rootFade : MonoBehaviour
 {
     public Text text;
-    private bool isStop;
-    bool isDown;
-    bool isUp;
-    float a;
+    public float a;
+    public bool isStop;
+    public bool isDown;
+    public bool isUp;
 
     void Start()
     {
         StartCoroutine(RootFade());
+        a = text.color.a;
+        Debug.Log(text.color.r+ text.color.g+ text.color.b+ text.color.a);
     }
     private void OnDisable()
     {
@@ -21,38 +23,38 @@ public class rootFade : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.anyKeyDown) isStop = true;
+        if (Input.anyKeyDown)
+        {
+            isStop = true;
+            StopCoroutine(RootFade());
+        }
     }
 
     IEnumerator RootFade()
     {
+        Debug.Log(text.color.a);
         float timer = 0;
-
         while (true)
         {
             yield return null;
-            timer += Time.unscaledDeltaTime * 1.5f;
-            if (timer % 2 == 0) isUp = true;
-
-            if (text.color.a == 255.0f)
+            if (text.color.a == 1.0f)
             {
-                //이제 내려가야함 
+                timer = 0.0f;
                 isDown = true;
                 isUp = false;
-                timer = 0.0f;
             }
             else if (text.color.a == 0.0f)
             {
-                //이제 올라가야함
-                isUp = true;
-                isDown = false;
                 timer = 0.0f;
+                isDown = false;
+                isUp = true;
             }
+            timer += Time.unscaledDeltaTime * 0.65f;
 
             if (isDown) a = Mathf.Lerp(1f, 0f, timer);
-            else if(isUp) a = Mathf.Lerp(0f, 1f, timer);
-            text.color = new Color(255, 255, 255, a);
+            else if (isUp) a = Mathf.Lerp(0f, 1f, timer);
 
+            text.color = new Color(0, 0, 0, a);
             if (isStop) break;
         }
     }
