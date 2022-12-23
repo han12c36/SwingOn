f81d84997949e67cfe76bc4311706d169e5ace45
han12c36 <112372556+han12c36@users.noreply.github.com>
2022-12-23 17:01:40 +0900

    Update
    Update

--- SwingOn/Assets/SwingOn/Scripts/SceneCtrl/rootFade.cs
+++ SwingOn/Assets/SwingOn/Scripts/SceneCtrl/rootFade.cs
@@ -7,6 +7,9 @@ public class rootFade : MonoBehaviour
 {
     public Text text;
     private bool isStop;
+    bool isDown;
+    bool isUp;
+    float a;
 
     void Start()
     {
@@ -24,25 +27,32 @@ public class rootFade : MonoBehaviour
     IEnumerator RootFade()
     {
         float timer = 0;
-        float a;
+
         while (true)
         {
             yield return null;
-            timer += Time.unscaledDeltaTime * 0.5f;
-            if (timer > 2.0f) timer = 0.0f;
-            else
+            timer += Time.unscaledDeltaTime * 1.5f;
+            if (timer % 2 == 0) isUp = true;
+
+            if (text.color.a == 255.0f)
             {
-                if(timer <= 1.0f)
-                {
-                    float a1 = Mathf.Lerp(1f, 0f, timer);
-                    text.color = new Color(255, 255, 255, a1);
-                }
-                else
-                {
-                    float a2 = Mathf.Lerp(0f, 1f, timer); 
-                    text.color = new Color(255, 255, 255, a2);
-                }
+                //이제 내려가야함 
+                isDown = true;
+                isUp = false;
+                timer = 0.0f;
             }
+            else if (text.color.a == 0.0f)
+            {
+                //이제 올라가야함
+                isUp = true;
+                isDown = false;
+                timer = 0.0f;
+            }
+
+            if (isDown) a = Mathf.Lerp(1f, 0f, timer);
+            else if(isUp) a = Mathf.Lerp(0f, 1f, timer);
+            text.color = new Color(255, 255, 255, a);
+
             if (isStop) break;
         }
     }
