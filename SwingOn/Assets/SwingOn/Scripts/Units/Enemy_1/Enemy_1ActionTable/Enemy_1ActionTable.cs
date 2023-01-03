593f9c104bba5ad0bf83accd8d3c9157ad4156ab
YoungSeok <112372556+han12c36@users.noreply.github.com>
2023-01-03 17:41:47 +0900

    Revert "Update"
    This reverts commit 4392916849049eb96cb6c9066b7a4ecf4fcc1dd3.

--- SwingOn/Assets/SwingOn/Scripts/Units/Enemy_1/Enemy_1ActionTable/Enemy_1ActionTable.cs
+++ SwingOn/Assets/SwingOn/Scripts/Units/Enemy_1/Enemy_1ActionTable/Enemy_1ActionTable.cs
@@ -78,10 +78,6 @@ public class Enemy_1ActionTable : ActionTable<Enemy_1>
         curAction_e = (Enums.Enemy_1Actions)curAction_i;
     }
 
-
-
-
-
     public void OnOffWeaponCollider_Enemy(int value)
     {
         if (value == 0) owner.EnemyWeapon.OnOffWeaponCollider(true);
